9057097a82f7018519d8a865fef0b699c3e55e53
Triphon Penakov <triphon@gmail.com>
2019-09-04 11:12:43 +0300

    org.otaibe.commons.quarkus.core.utils.BeanUtils.deepClone

---
+++ otaibe-commons-quarkus-core/src/main/java/org/otaibe/commons/quarkus/core/utils/BeanUtils.java
@@ -0,0 +1,38 @@
+package org.otaibe.commons.quarkus.core.utils;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.io.*;
+
+/**
+ * Created by triphon on 11.11.16.
+ */
+@ApplicationScoped
+@Slf4j
+public class BeanUtils {
+
+    public <T extends Serializable> T deepClone(T input) {
+        if (input == null) {
+            return null;
+        }
+
+        try {
+            try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()){
+                try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream)){
+                    objectOutputStream.writeObject(input);
+                }
+
+                try (ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray())){
+                    try (ObjectInputStream objectInputStream = new ObjectInputStream(inputStream)){
+                        T res = (T) objectInputStream.readObject();
+                        return res;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            log.error("unable to deep clone object", e);
+            throw new RuntimeException(e);
+        }
+    }
+}
