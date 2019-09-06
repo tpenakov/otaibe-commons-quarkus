b2ef2eb0aa308b775edd392a4ffcbab5cda0adfc
Triphon Penakov <triphon@gmail.com>
2019-09-06 16:52:46 +0300

    add ability to set reactive mongo client externally

--- otaibe-commons-quarkus-mongodb-core/src/main/java/org/otaibe/commons/quarkus/mongodb/core/service/MongoDbClientService.java
+++ otaibe-commons-quarkus-mongodb-core/src/main/java/org/otaibe/commons/quarkus/mongodb/core/service/MongoDbClientService.java
@@ -3,9 +3,11 @@ package org.otaibe.commons.quarkus.mongodb.core.service;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
 import lombok.Getter;
+import lombok.Setter;
 import org.otaibe.commons.quarkus.mongodb.core.domain.MongoDbSettings;
 
 @Getter
+@Setter
 public abstract class MongoDbClientService {
 
     MongoClient client;
