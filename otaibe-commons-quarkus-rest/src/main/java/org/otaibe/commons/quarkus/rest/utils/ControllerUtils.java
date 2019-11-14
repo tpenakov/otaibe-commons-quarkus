9ac8511dcb046f1d4433f5f03cfbbcff67840ad6
Triphon Penakov <triphon@gmail.com>
2019-11-14 10:15:50 +0200

    ControllerUtils - handle global exceptions and added custom client error

--- otaibe-commons-quarkus-rest/src/main/java/org/otaibe/commons/quarkus/rest/utils/ControllerUtils.java
+++ otaibe-commons-quarkus-rest/src/main/java/org/otaibe/commons/quarkus/rest/utils/ControllerUtils.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuples;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.core.Response;
@@ -16,27 +17,47 @@ import java.util.function.Function;
 @Setter
 @Slf4j
 public class ControllerUtils {
+
+    public static final String CLIENT_ERROR_KEY = "client-error-key";
+
     public <T> Mono<T> processResult(CompletableFuture<Response> webResult,
                                      Mono<T> result,
                                      T defaultIfEmpty,
                                      Function<T, Boolean> isValid) {
-        return result
-                .defaultIfEmpty(defaultIfEmpty)
-                .doOnNext(t1 -> Optional.ofNullable(t1)
-                        .filter(t -> isValid.apply(t))
-                        .map(t -> webResult.complete(Response.ok(t).build()))
-                        .orElseGet(() -> webResult.complete(
-                                buildErrorResponse(Response.Status.BAD_REQUEST,
-                                        Response.Status.BAD_REQUEST.getReasonPhrase()))
-                        ))
-                .doOnError(throwable -> webResult.complete(
-                        buildErrorResponse(
-                                Response.Status.INTERNAL_SERVER_ERROR,
-                                throwable.getMessage())))
-                .doOnTerminate(() -> log.debug("processResult end."));
+        try {
+            return result
+                    .defaultIfEmpty(defaultIfEmpty)
+                    .flatMap(t -> Mono.subscriberContext()
+                            .map(context -> context.getOrEmpty(CLIENT_ERROR_KEY))
+                            .map(o -> Tuples.of(t, o))
+                    )
+                    .doOnNext(objects -> Optional.ofNullable(objects.getT1())
+                            .filter(t -> isValid.apply(t))
+                            .map(t -> webResult.complete(Response.ok(t).build()))
+                            .orElseGet(() -> {
+                                Response.Status status = objects.getT2().map(o -> (Response.Status) o).orElse(Response.Status.BAD_REQUEST);
+                                return webResult.complete(
+                                        buildErrorResponse(status,
+                                                status.getReasonPhrase()));
+                                    }
+                            ))
+                    .map(objects -> objects.getT1())
+                    .doOnError(throwable -> webResult.complete(
+                            buildErrorResponse(
+                                    Response.Status.INTERNAL_SERVER_ERROR,
+                                    throwable.getMessage())))
+                    .doOnTerminate(() -> log.debug("processResult end."));
+        } catch (Exception e) {
+            log.error("unhandled exception", e);
+            webResult.complete(
+                    buildErrorResponse(
+                            Response.Status.INTERNAL_SERVER_ERROR,
+                            e.getMessage()));
+            return Mono.just(defaultIfEmpty);
+        }
     }
 
-    private Response buildErrorResponse(Response.Status status, String message) {
+    public Response buildErrorResponse(Response.Status status, String message) {
         return Response.serverError()
                 .status(status)
                 .entity(message)
