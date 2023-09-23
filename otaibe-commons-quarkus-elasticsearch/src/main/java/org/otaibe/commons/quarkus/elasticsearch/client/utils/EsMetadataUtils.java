c5b0f75b228e2fd6eb4bd63353b22f921f07f175
triphon <triphon@gmail.com>
2023-09-23 14:18:50 +0300

    Upgrading to quarkus 3.4.1 - now is compiled

--- otaibe-commons-quarkus-elasticsearch/src/main/java/org/otaibe/commons/quarkus/elasticsearch/client/utils/EsMetadataUtils.java
+++ otaibe-commons-quarkus-elasticsearch/src/main/java/org/otaibe/commons/quarkus/elasticsearch/client/utils/EsMetadataUtils.java
@@ -1,17 +1,16 @@
 package org.otaibe.commons.quarkus.elasticsearch.client.utils;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.search.sort.SortOrder;
 import org.otaibe.commons.quarkus.elasticsearch.client.domain.EsMetadata;
 import reactor.util.context.Context;
-
-import javax.enterprise.context.ApplicationScoped;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
+import reactor.util.context.ContextView;
 
 @Getter
 @Setter
@@ -20,37 +19,37 @@ public class EsMetadataUtils {
 
     public static final String METADATA = "metadata";
 
-    public Optional<EsMetadata> extract(Context context) {
+    public Optional<EsMetadata> extract(final ContextView context) {
         return context.getOrEmpty(METADATA);
     }
 
-    public Context put(Context context, EsMetadata entity) {
+    public Context put(final Context context, final EsMetadata entity) {
         return context.put(METADATA, entity);
     }
 
-    public void setQueryMetadataFrom(EsMetadata entity, Integer value) {
+    public void setQueryMetadataFrom(final EsMetadata entity, final Integer value) {
         ensureQueryMetadata(entity).setFrom(value);
     }
 
-    public void setQueryMetadataSize(EsMetadata entity, Integer value) {
+    public void setQueryMetadataSize(final EsMetadata entity, final Integer value) {
         ensureQueryMetadata(entity).setSize(value);
     }
 
-    public void setQueryMetadataAskForScrollId(EsMetadata entity, Boolean value) {
+    public void setQueryMetadataAskForScrollId(final EsMetadata entity, final Boolean value) {
         ensureQueryMetadata(entity).setAskForScrollId(value);
     }
 
-    public void setQueryMetadataIsOpTypeCreate(EsMetadata entity, Boolean value) {
+    public void setQueryMetadataIsOpTypeCreate(final EsMetadata entity, final Boolean value) {
         ensureQueryMetadata(entity).setIsOpTypeCreate(value);
     }
 
-    public void addQueryMetadataSort(EsMetadata entity, String key, SortOrder value) {
+    public void addQueryMetadataSort(final EsMetadata entity, final String key, final SortOrder value) {
         ensureQueryMetadataSort(entity).put(key, value);
         return;
     }
 
-    public Map<String, SortOrder> ensureQueryMetadataSort(EsMetadata entity) {
-        EsMetadata.EsQueryMetadata esQueryMetadata = ensureQueryMetadata(entity);
+    public Map<String, SortOrder> ensureQueryMetadataSort(final EsMetadata entity) {
+        final EsMetadata.EsQueryMetadata esQueryMetadata = ensureQueryMetadata(entity);
         return Optional.ofNullable(esQueryMetadata.getSort())
                 .orElseGet(() -> {
                     esQueryMetadata.setSort(new LinkedHashMap<>());
@@ -59,7 +58,7 @@ public class EsMetadataUtils {
                 ;
     }
 
-    public EsMetadata.EsQueryMetadata ensureQueryMetadata(EsMetadata entity) {
+    public EsMetadata.EsQueryMetadata ensureQueryMetadata(final EsMetadata entity) {
         return Optional.ofNullable(entity.getQuery())
                 .orElseGet(() -> {
                     entity.setQuery(new EsMetadata.EsQueryMetadata());
@@ -67,7 +66,7 @@ public class EsMetadataUtils {
                 });
     }
 
-    public Map<String, EsMetadata.EsDaoMetadata> ensureDaoMap(EsMetadata entity) {
+    public Map<String, EsMetadata.EsDaoMetadata> ensureDaoMap(final EsMetadata entity) {
         return Optional.ofNullable(entity.getDaoMap())
                 .orElseGet(() -> {
                     entity.setDaoMap(new ConcurrentHashMap<>());
